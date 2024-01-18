b18b76097e02a9a6f52107751d8e5c548f91bd74
Andrea Sciagura <andrea@tappable.co.uk>
2024-01-18 10:51:22 +0000

    feat(UI): Made show bottom sheet when expanded

--- app/feedback/src/main/java/uk/co/tappable/feedback/Feedback.kt
+++ app/feedback/src/main/java/uk/co/tappable/feedback/Feedback.kt
@@ -1,21 +1,17 @@
 package uk.co.tappable.feedback
 
-import androidx.compose.animation.Animatable
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.animateOffsetAsState
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BugReport
@@ -23,6 +19,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
@@ -33,8 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
@@ -45,7 +40,7 @@ import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
 
 
-private const val minSpacing = 20
+private const val minSpacing = 8
 
 @Composable
 fun Feedback(modifier: Modifier = Modifier, content: @Composable (Modifier) -> Unit) {
@@ -56,9 +51,12 @@ fun Feedback(modifier: Modifier = Modifier, content: @Composable (Modifier) -> U
     val spacing = remember(context) { with(density) { minSpacing.dp.toPx() } }
     val screenHeight = remember(context) { displayMetrics.heightPixels }
     val screenWidth = remember(context) { displayMetrics.widthPixels }
-    var offsetX by rememberSaveable { mutableFloatStateOf(screenWidth / 2f) }
-    var offsetY by rememberSaveable { mutableFloatStateOf(screenHeight / 2f) }
-    val offset = animateOffsetAsState(targetValue = Offset(offsetX, offsetY), label = "debug")
+    var offsetX by rememberSaveable { mutableFloatStateOf(0f) }
+    var offsetY by rememberSaveable { mutableFloatStateOf(0f) }
+    val offset = animateOffsetAsState(
+        targetValue = Offset(if (expanded) 5f else offsetX, if (expanded) 5f else offsetY),
+        label = "debug"
+    )
     val blur = animateDpAsState(targetValue = if (expanded) 10.dp else 0.dp, label = "blur")
 
     var fabSize by remember { mutableStateOf(IntSize(0, 0)) }
@@ -72,12 +70,16 @@ fun Feedback(modifier: Modifier = Modifier, content: @Composable (Modifier) -> U
             exit = slideOutVertically(targetOffsetY = { it + 50 }),
         ) {
             //show an full detailed ui with a text field and a button
-            Box(
-                Modifier
-                    .matchParentSize()
-                    .background(Color.Black.copy(alpha = 0.5f))
-                    .blur(10.dp)
-            )
+            Surface(
+                modifier = Modifier
+                    .statusBarsPadding()
+                    .systemBarsPadding()
+                    .padding(top = with(density) { (fabSize.height / 2).toDp() })
+                    .matchParentSize(),
+                color = MaterialTheme.colorScheme.onSurface
+            ) {
+                //TODO: add content to debug menu
+            }
         }
         //show a floating button above the app content
 
@@ -89,6 +91,11 @@ fun Feedback(modifier: Modifier = Modifier, content: @Composable (Modifier) -> U
             modifier = Modifier
                 .onGloballyPositioned {
                     fabSize = it.size
+                    if (offset.value == Offset.Zero) {
+                        //update position to half screen
+                        offsetX = screenWidth.toFloat() / 2
+                        offsetY = screenHeight.toFloat() / 2
+                    }
                 }
                 .offset {
                     IntOffset(
@@ -99,22 +106,27 @@ fun Feedback(modifier: Modifier = Modifier, content: @Composable (Modifier) -> U
                     )
                 }
                 .pointerInput(Unit) {
-                    detectDragGestures(onDrag = { change, dragAmount ->
-                        change.consume()
+                    if (!expanded) {
+                        detectDragGestures(
+                            onDrag = { change, dragAmount ->
+                                change.consume()
 
-                        offsetX += dragAmount.x
-                        offsetY += dragAmount.y
-                    }, onDragEnd = {
-                        offsetX = if (offsetX <= screenWidth / 2)
-                            fabSize.width
-                                .div(2)
-                                .times(-1)
-                                .plus(spacing)
-                        else
-                            screenWidth.toFloat() - fabSize.width
-                                .div(2)
-                                .plus(spacing)
-                    })
+                                offsetX += dragAmount.x
+                                offsetY += dragAmount.y
+                            },
+                            onDragEnd = {
+                                offsetX = offsetX.roundToNearestValue(
+                                    lowEnd = fabSize.width
+                                        .div(2)
+                                        .times(-1)
+                                        .plus(spacing),
+                                    highEnd = screenWidth.toFloat() - fabSize.width
+                                        .div(2)
+                                        .plus(spacing)
+                                )
+                            },
+                        )
+                    }
                 },
             shape = CircleShape,
             contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
@@ -127,4 +139,10 @@ fun Feedback(modifier: Modifier = Modifier, content: @Composable (Modifier) -> U
             },
         )
     }
+}
+
+fun Float.roundToNearestValue(lowEnd: Float, highEnd: Float): Float {
+    val average = (lowEnd + highEnd) / 2
+    return if (this <= average) lowEnd
+    else highEnd
 }
